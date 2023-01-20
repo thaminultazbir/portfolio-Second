806bb53f4d958290ee14504ea9c7cafb673e54d4
thaminultazbir <thaminultazbir1999@gmail.com>
2023-01-21 04:20:07 +0600

    home section done

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Social() {
+  return (
+    <div className="home_social">
+        <a href="https://www.facebook.com/asif.tazbir/" className="home_social-icon" target="_blank" rel="noreferrer">
+        <i class="uil uil-facebook-f"></i>
+        </a>
+
+        <a href="https://github.com/thaminultazbir" className="home_social-icon" target="_blank" rel="noreferrer">
+        <i class="uil uil-github"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/thaminul-tazbir-1b8778228/" className="home_social-icon" target="_blank" rel="noreferrer">
+        <i class="uil uil-linkedin"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
