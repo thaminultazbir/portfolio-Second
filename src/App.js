45d9caf82d09af88ceb7b921bf55e5f40e8217cc
thaminultazbir <thaminultazbir1999@gmail.com>
2023-01-23 03:21:30 +0600

    about section done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <Home/>
+      <main className='main'>
+        <Home />
+        <About />
       </main>
     </>
   );
